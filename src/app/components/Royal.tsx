f9a11bd5a2ffe7ee7f798a207b1d4c8b14d05761
Hubert <selormeyh@gmail.com>
2024-10-22 14:53:05 +0000

    rt

---
+++ src/app/components/Royal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Royal.module.css'
+
+const Royal = () => {
+  return (
+    <div className={styles.main}>This is Homepage</div>
+  )
+}
+
+export default Royal
